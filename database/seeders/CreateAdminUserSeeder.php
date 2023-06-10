04f60eee263a6a40c7eb709ee0fa1ee7ad9715e6
adilkhan12324 <adilkhan12324@gmail.com>
2023-06-10 21:33:02 +0500

    roles and permissions

---
+++ database/seeders/CreateAdminUserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //Admin Seeder
+        $user = User::create([
+            'name' => 'LaravelTuts', 
+            'email' => 'admin@laraveltuts.com',
+            'password' => bcrypt('password')
+        ]);
+      
+        $role = Role::create(['name' => 'Admin']);
+       
+        $permissions = Permission::pluck('id','id')->all();
+     
+        $role->syncPermissions($permissions);
+       
+        $user->assignRole([$role->id]);
+    }
+}
